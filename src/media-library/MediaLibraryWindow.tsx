835ea2ac877500621abe15e3d2d57643e3384fc7
Jay Meistrich <jmeistrich@gmail.com>
2025-10-31 19:49:52 +0000

    fix: pressing hotkeys in MediaLibrary was making a system sound

--- src/media-library/MediaLibraryWindow.tsx
+++ src/media-library/MediaLibraryWindow.tsx
@@ -6,22 +6,29 @@ import { DragDropProvider } from "@/components/dnd";
 import { MediaLibraryView } from "@/components/MediaLibrary";
 import { TooltipProvider } from "@/components/TooltipProvider";
 import { ThemeProvider } from "@/theme/ThemeProvider";
+import { HiddenTextInput } from "@/systems/keyboard/HookKeyboard";
+import { WindowProvider } from "@/windows";
+
+const MEDIA_LIBRARY_WINDOW_ID = "media-library";
 
 export function MediaLibraryWindow() {
     return (
-        <VibrancyView blendingMode="behindWindow" material="sidebar" style={styles.vibrancy}>
-            <ThemeProvider>
-                <PortalProvider>
-                    <View className="flex-1 bg-background-primary/60 min-h-full">
-                        <TooltipProvider>
-                            <DragDropProvider>
-                                <MediaLibraryView />
-                            </DragDropProvider>
-                        </TooltipProvider>
-                    </View>
-                </PortalProvider>
-            </ThemeProvider>
-        </VibrancyView>
+        <WindowProvider id={MEDIA_LIBRARY_WINDOW_ID}>
+            <VibrancyView blendingMode="behindWindow" material="sidebar" style={styles.vibrancy}>
+                <ThemeProvider>
+                    <HiddenTextInput />
+                    <PortalProvider>
+                        <View className="flex-1 bg-background-primary/60 min-h-full">
+                            <TooltipProvider>
+                                <DragDropProvider>
+                                    <MediaLibraryView />
+                                </DragDropProvider>
+                            </TooltipProvider>
+                        </View>
+                    </PortalProvider>
+                </ThemeProvider>
+            </VibrancyView>
+        </WindowProvider>
     );
 }
 
