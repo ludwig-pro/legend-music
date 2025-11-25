f5a1921e875a5c2fda0f28607496908942d4b614
Jay Meistrich <github@jayz.us>
2025-11-25 20:53:26 +0000

    legend state batch the cache preloads

--- src/utils/preloadPersistence.ts
+++ src/utils/preloadPersistence.ts
@@ -1,11 +1,14 @@
+import { batch } from "@legendapp/state";
 import { getPlaylistCacheSnapshot } from "@/systems/PlaylistCache";
 import { settings$ } from "@/systems/Settings";
 import { stateSaved$ } from "@/systems/State";
 import { themeState$ } from "@/theme/ThemeProvider";
 
 export function preloadPersistence() {
-    getPlaylistCacheSnapshot();
-    settings$.get();
-    stateSaved$.get();
-    themeState$.get();
+    batch(() => {
+        getPlaylistCacheSnapshot();
+        settings$.get();
+        stateSaved$.get();
+        themeState$.get();
+    });
 }
